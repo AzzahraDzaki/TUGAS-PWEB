f4d2b31a2109b0a6a0cdba485f016fe7827e4b71
AzzahraDzaki <athifaha96@gmail.com>
2023-04-10 12:53:27 +0700

    menambahkan file koneksi

---
+++ koneksi.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+
+//buat koneksi database
+const connect = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : '',
+    database : 'db_pweb1'
+});
+
+connect.connect((err)=>{
+    if(err) throw err;
+    console.log('Mysql terkoneksi');
+});
+
+module.export = conn;
